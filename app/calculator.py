422783fcc33273f750b49fe94bdc2b65b1aa9258
sithembisok <sithembiso@lepsta.tech>
2020-12-15 17:09:05 +0200

    message: Test changes state: states/master/bugfix

--- app/calculator.py
+++ app/calculator.py
@@ -2,6 +2,8 @@ class Calculator(object):
     def add(self, x, y):
         number_types = (int, long, float, complex)
  
+        # always write comments
+        # __init__.py??
         if isinstance(x, number_types) and isinstance(y, number_types):
             return x + y
         else:
